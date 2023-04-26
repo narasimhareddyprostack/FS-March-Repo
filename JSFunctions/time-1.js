c92a2061f84e5a6f861f4c52e51b00620f294090
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-04-26 12:06:29 +0530

    okie

---
+++ JSFunctions/time-1.js
@@ -0,0 +1,9 @@
+//setTimeout(1,2);
+setTimeout(() => {
+    console.log("GM")
+    console.log("GA")
+    console.log("GN")
+}, 5000);
+/*
+    is function, executing provided function after 1 sec
+*/
